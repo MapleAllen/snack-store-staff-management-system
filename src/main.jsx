7dbe1fc0664482f6c48c42bfcfdd7eed7def07a8
scyzx5 <2891085213@qq.com>
2026-08-01 01:26:09 +0800

    build: install antd and setup global ConfigProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { ConfigProvider } from "antd";
+import zhCN from "antd/locale/zh_CN";
 import { App } from "./App.jsx";
 import "./styles.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider
+      locale={zhCN}
+      theme={{
+        token: {
+          colorPrimary: "#1677ff",
+          borderRadius: 8,
+        },
+      }}
+    >
+      <App />
+    </ConfigProvider>
   </React.StrictMode>,
 );
+
